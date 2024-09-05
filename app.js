734c93ae2c0fa423d94fa5e97aecaef50265c67c
Pedro Henrique Vieira Barreto <112330570+Masterbarreto@users.noreply.github.com>
2024-09-05 10:05:22 -0300

    Correção

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
+import { dadosFilmes } from './dados.js';
 
-function pesquisa() {
-    console.log('master')
-}
+// function pesquisa() {
+//     console.log('master')
+// }
 
-let section = document.getElementById('Resultados-pesquisa'); 
+document.querySelector("#botao-pesquisar").addEventListener("click", () => {
+    let section = document.getElementById('search-results');
 console.log(dadosFilmes[0].genero);
-let Resultados=''
+let Resultados = ''
 
 for (let filme of dadosFilmes) {
-    Resultados += 
-    <div class="card">
-        <h2>${filme.titulo}</h2>
-        <p><strong>Ano:</strong> ${filme.ano}</p>
-        <p><strong>Gênero:</strong> ${filme.genero}</p>
-        <p><strong>Diretor:</strong> ${filme.diretor}</p>
-        <p><strong>Atores:</strong> ${filme.atores.join(', ')}</p>
-        <p><strong>Descrição:</strong> ${filme.descricao}</p>
-        <p><strong>Plataforma:</strong> ${filme.plataforma}</p>
-        <p><strong>Duração:</strong> ${filme.duração} minutos</p>
-        <p><strong>Idioma original:</strong> ${filme.idiomaOriginal}</p>
-    </div>
-    ;
+    Resultados +=
+        `<div class="card">
+            <h2>${filme.titulo}</h2>
+            <p><strong>Ano:</strong> ${filme.ano}</p>
+            <p><strong>Gênero:</strong> ${filme.genero}</p>
+            <p><strong>Diretor:</strong> ${filme.diretor}</p>
+            <p><strong>Atores:</strong> ${filme.atores.join(', ')}</p>
+            <p><strong>Descrição:</strong> ${filme.descricao}</p>
+            <p><strong>Plataforma:</strong> ${filme.plataforma}</p>
+            <p><strong>Duração:</strong> ${filme.duração} minutos</p>
+            <p><strong>Idioma original:</strong> ${filme.idiomaOriginal}</p>
+        </div>`
+        ;
 }
 
 section.innerHTML = Resultados;
+})
+
