05b2bb30e74389eee6b6cd833da0af906a40e34f
Pedro Henrique Vieira Barreto <112330570+Masterbarreto@users.noreply.github.com>
2024-09-05 09:25:24 -0300

    aula

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-import { dadosFilmes} from './dados.js';
+import { dadosFilmes } from './dados.js';
 
+function pesquisa() {
+    console.log('master')
+}
+
+let section = document.getElementById('Resultados-pesquisa'); 
 console.log(dadosFilmes[0].genero);
+let Resultados=''
 
+for (let filme of dadosFilmes) {
+    Resultados += `
+    <div class="card">
+        <h2>${filme.titulo}</h2>
+        <p><strong>Ano:</strong> ${filme.ano}</p>
+        <p><strong>Gênero:</strong> ${filme.genero}</p>
+        <p><strong>Diretor:</strong> ${filme.diretor}</p>
+        <p><strong>Atores:</strong> ${filme.atores.join(', ')}</p>
+        <p><strong>Descrição:</strong> ${filme.descricao}</p>
+        <p><strong>Plataforma:</strong> ${filme.plataforma}</p>
+        <p><strong>Duração:</strong> ${filme.duração} minutos</p>
+        <p><strong>Idioma original:</strong> ${filme.idiomaOriginal}</p>
+    </div>
+    `;
+}
+
+section.innerHTML = Resultados;
